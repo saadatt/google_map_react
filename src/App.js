b3587212ccec187a0225204933fc2ae04fc9a478
Sadat <saadatt@gmail.com>
2020-04-20 17:19:41 -0500

    updated setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
-/*import Logo from "./components/Logo";
-import Menu from "./components/Menu";
-import MenuItem from "./components/MenuItem";*/
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./views/LandingPage";
+
 function App() {
-  return (
-      <div>
-        <div className='container'>
-          <div className="topnav">
-            <Header/>
-          </div>
-          <Content/>
-          <Footer/>
-        </div>
-      </div>
-  );
+    return (
+        <Switch>
+            <Route exact path="/" component={LandingPage} />
+        </Switch>
+    );
 }
 
 export default App;
+
